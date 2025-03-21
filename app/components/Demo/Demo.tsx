3e075302652a95a6fd264221bd8086483af1c7aa
Giraphi <hoeps.raphael@gmail.com>
2025-03-21 13:33:30 +0100

    feat: bandcamp layout

--- app/components/Demo/Demo.tsx
+++ app/components/Demo/Demo.tsx
@@ -1,7 +1,6 @@
 import ContentCol from "@/components/ContentCol"
 import GlitchText from "@/components/GlitchText/GlitchText"
 import Grid from "@/components/Grid"
-import { cn } from "@/lib/utils"
 import Glyphs from "@/svg/glyphs.svg"
 
 export interface DemoProps {
@@ -26,7 +25,8 @@ export default function Demo({ className }: DemoProps) {
           {'"Recording", "Mixing"'} und Gestaltung all homemade by ubu.zip.
         </p>
       </ContentCol>
-      <div className="col-span-full flex w-full items-center justify-center md:col-span-10 md:col-start-2 md:justify-between xl:col-span-8 xl:col-start-3">
+
+      <div className="col-span-full hidden w-full items-center justify-center md:col-span-10 md:col-start-2 md:flex md:justify-between xl:col-span-8 xl:col-start-3">
         <Glyphs className="hidden h-[400px] md:block lg:h-[500px]" />
 
         <iframe
@@ -36,6 +36,15 @@ export default function Demo({ className }: DemoProps) {
           className="h-[530px] w-[230px] border-0 lg:h-[677px] lg:w-[400px]"
         />
         <Glyphs className="bg-red hidden h-[400px] md:block lg:h-[500px]" />
+      </div>
+
+      <div className="col-span-full flex w-full items-center justify-center md:hidden">
+        <iframe
+          title={"bandcamp"}
+          src="https://bandcamp.com/EmbeddedPlayer/album=591127691/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/"
+          seamless
+          className="h-[419px] w-[300px] border-0"
+        />
       </div>
     </Grid>
   )
