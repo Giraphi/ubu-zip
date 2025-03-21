fc5209616b4193f9176cf537d13dbfc53e2179bf
Giraphi <hoeps.raphael@gmail.com>
2025-03-21 18:48:47 +0100

    feat: beast animation

--- app/components/Intro/Beast.tsx
+++ app/components/Intro/Beast.tsx
@@ -7,7 +7,7 @@ import { motion, useAnimation } from "motion/react"
 import { useEffect } from "react"
 import { useMediaQuery } from "react-responsive"
 
-const repeatsLg = Array.from({ length: 10 }, () => [10, -10]).flat()
+const repeatsLg = Array.from({ length: 13 }, () => [10, -10]).flat()
 const repeatsSm = Array.from({ length: 10 }, () => [7, -7]).flat()
 
 export default function Beast() {
@@ -20,7 +20,7 @@ export default function Beast() {
         x: ["100%", "0%"],
         y: [0, ...repeatsLg, 0],
         transition: {
-          duration: 7,
+          duration: 8,
           repeat: Infinity,
           repeatType: "reverse",
           ease: "easeInOut",
@@ -42,17 +42,15 @@ export default function Beast() {
 
   return (
     <Grid>
-      <div className="col-span-full md:col-span-10 md:col-start-2 xl:col-span-8 xl:col-start-3">
-        <div className="relative flex justify-end overflow-hidden pt-8 md:pb-4">
-          <div className="mr-28 w-full md:mr-36">
-            <motion.div
-              className="h-48"
-              initial={{ x: "100%" }}
-              animate={controls}
-            >
-              <BeastSVG className="w-28 md:w-36" />
-            </motion.div>
-          </div>
+      <div className="relative col-span-full flex justify-end overflow-hidden pt-8 md:col-span-10 md:col-start-2 md:pb-4 xl:col-span-8 xl:col-start-3">
+        <div className="b-8 mr-28 w-full md:mr-36">
+          <motion.div
+            className="h-48"
+            initial={{ x: "100%" }}
+            animate={controls}
+          >
+            <BeastSVG className="w-28 md:w-36" />
+          </motion.div>
         </div>
       </div>
     </Grid>
