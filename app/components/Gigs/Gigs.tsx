cc0190cc95b36a7f9366b90fdfea7ee17ad2c4a2
Giraphi <hoeps.raphael@gmail.com>
2025-10-22 19:47:16 +0200

    node 22

--- app/components/Gigs/Gigs.tsx
+++ app/components/Gigs/Gigs.tsx
@@ -24,45 +24,45 @@ export default function Gigs({ className }: GigsProps) {
       <div className="col-span-full flex justify-center pb-8 md:col-span-8 md:col-start-3 md:pb-0 lg:col-span-4">
         <div className="grid grid-cols-[auto_1fr] gap-x-1.5">
           <div className="contents">
-            <span>01.11. - </span>
+            <span>01.11.25 - </span>
             <span>Loge. Berlin</span>
           </div>
           <div className="contents">
-            <span>05.09. - </span>
+            <span>05.09.25 - </span>
             <span>Sunny Red, München</span>
           </div>
           <div className="contents">
-            <span>13.06. - </span>
+            <span>13.06.25 - </span>
             <span>Glockenbachwerkstatt, München</span>
           </div>
           <div className="contents">
-            <span>21.05. - </span>
+            <span>21.05.25 - </span>
             <span>Import Export, München</span>
           </div>
 
           <div className="contents">
-            <span>12.05. - </span>
+            <span>12.05.25 - </span>
             <span>Favorit Bar, München</span>
           </div>
 
           <div className="contents">
-            <span>04.05. - </span>
+            <span>04.05.25 - </span>
             <span>Bello Bar, Dublin IR</span>
           </div>
           <div className="contents">
-            <span>03.05. - </span>
+            <span>03.05.25 - </span>
             <span>Fred Zeppelin&apos;s, Cork IR</span>
           </div>
           <div className="contents">
-            <span>02.05. - </span>
+            <span>02.05.25 - </span>
             <span>The Kasbah, Limerick IR</span>
           </div>
           <div className="contents">
-            <span>01.05. - </span>
+            <span>01.05.25 - </span>
             <span>The Deer Shead, Belfast IR</span>
           </div>
           <div className="contents">
-            <span>28.03. - </span>
+            <span>28.03.25 - </span>
             <span>Walter Storms Gallerie, München</span>
           </div>
         </div>
