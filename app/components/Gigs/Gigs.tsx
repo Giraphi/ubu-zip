a952a38800b9e7674e2e2ddc6655fd299beac92d
Giraphi <hoeps.raphael@gmail.com>
2026-06-03 10:50:19 +0200

    gigs

--- app/components/Gigs/Gigs.tsx
+++ app/components/Gigs/Gigs.tsx
@@ -24,47 +24,83 @@ export default function Gigs({ className }: GigsProps) {
       <div className="col-span-full flex justify-center pb-8 md:col-span-8 md:col-start-3 md:pb-0 lg:col-span-4">
         <div className="grid grid-cols-[auto_1fr] gap-x-1.5">
           <div className="contents">
-            <span>01.11.25 - </span>
-            <span>Loge. Berlin</span>
+            <span>02.10.26 - </span>
+            <span>Rhiz, Wien</span>
           </div>
           <div className="contents">
-            <span>05.09.25 - </span>
-            <span>Sunny Red, München</span>
+            <span>26.09.26 - </span>
+            <span>Zeughaus, Passau. Mit Misss Mellow</span>
           </div>
           <div className="contents">
-            <span>13.06.25 - </span>
-            <span>Glockenbachwerkstatt, München</span>
+            <span>16.06.26 - </span>
+            <span>Glockenbachwerkstatt, München. Mit Nasty Neighbors</span>
           </div>
           <div className="contents">
-            <span>21.05.25 - </span>
-            <span>Import Export, München</span>
+            <span>04.06.26 - </span>
+            <span>Milla, München. Krauthammer</span>
+          </div>
+          <div className="contents">
+            <span>09.05.26 - </span>
+            <span>Walter Storms Galerie, München</span>
+          </div>
+          <div className="contents">
+            <span>07.03.26 - </span>
+            <span>Tamtam Treppenbar, München. Mit Golden Diskoship</span>
+          </div>
+          <div className="contents">
+            <span>06.03.26 - </span>
+            <span>Noch Besser Leben, Leipzig. Mit Mygolden Diskoship</span>
           </div>
 
-          <div className="contents">
-            <span>12.05.25 - </span>
-            <span>Favorit Bar, München</span>
-          </div>
-
-          <div className="contents">
-            <span>04.05.25 - </span>
-            <span>Bello Bar, Dublin IR</span>
-          </div>
-          <div className="contents">
-            <span>03.05.25 - </span>
-            <span>Fred Zeppelin&apos;s, Cork IR</span>
-          </div>
-          <div className="contents">
-            <span>02.05.25 - </span>
-            <span>The Kasbah, Limerick IR</span>
-          </div>
-          <div className="contents">
-            <span>01.05.25 - </span>
-            <span>The Deer Shead, Belfast IR</span>
-          </div>
-          <div className="contents">
-            <span>28.03.25 - </span>
-            <span>Walter Storms Gallerie, München</span>
-          </div>
+          <details className="col-span-2 mt-4">
+            <summary className="cursor-pointer">2025</summary>
+            <div className="mt-1.5 grid grid-cols-[auto_1fr] gap-x-1.5">
+              <div className="contents">
+                <span>19.12.25 - </span>
+                <span>Tamtam Treppenbar, München</span>
+              </div>
+              <div className="contents">
+                <span>01.11.25 - </span>
+                <span>Loge. Berlin</span>
+              </div>
+              <div className="contents">
+                <span>05.09.25 - </span>
+                <span>Sunny Red, München</span>
+              </div>
+              <div className="contents">
+                <span>13.06.25 - </span>
+                <span>Glockenbachwerkstatt, München</span>
+              </div>
+              <div className="contents">
+                <span>21.05.25 - </span>
+                <span>Import Export, München</span>
+              </div>
+              <div className="contents">
+                <span>12.05.25 - </span>
+                <span>Favorit Bar, München</span>
+              </div>
+              <div className="contents">
+                <span>04.05.25 - </span>
+                <span>Bello Bar, Dublin IR</span>
+              </div>
+              <div className="contents">
+                <span>03.05.25 - </span>
+                <span>Fred Zeppelin&apos;s, Cork IR</span>
+              </div>
+              <div className="contents">
+                <span>02.05.25 - </span>
+                <span>The Kasbah, Limerick IR</span>
+              </div>
+              <div className="contents">
+                <span>01.05.25 - </span>
+                <span>The Deer Shead, Belfast IR</span>
+              </div>
+              <div className="contents">
+                <span>28.03.25 - </span>
+                <span>Walter Storms Gallerie, München</span>
+              </div>
+            </div>
+          </details>
         </div>
       </div>
       <div className="col-span-2 col-start-2 md:col-span-2 md:col-start-11 lg:col-start-9">
