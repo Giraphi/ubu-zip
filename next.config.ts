26d510710507cece80bb8e5b18990b0f3ae6178a
Giraphi <hoeps.raphael@gmail.com>
2026-06-03 11:06:20 +0200

    upgrade next.js

--- next.config.ts
+++ next.config.ts
@@ -8,13 +8,11 @@ const nextConfig = {
     })
     return config
   },
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.ts",
-        },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.ts",
       },
     },
   },
